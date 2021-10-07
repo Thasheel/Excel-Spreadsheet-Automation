7a5efb7e53372a4a5f253ccc211c3cc3e4e9608a
Mohammed Thasheel <spidy9741@gmail.com>
2021-10-07 20:30:22 +0530

    loop added for getting values from xl

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ def process_workbook(filename):
     cell = sheet.cell(1, 1)  # rows and colums
     # print(sheet.max_row)  # for finding how many rows
     # print(cell.value)
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
