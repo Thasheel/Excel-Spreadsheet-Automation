d9932331f26678bea8086fa0d15de087e395a547
Mohammed Thasheel <spidy9741@gmail.com>
2021-10-07 20:29:16 +0530

    xl fuctions

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import openpyxl as xl
 
 def process_workbook(filename):
+    wb = xl.load_workbook(filename)  # importing file,use Tab for crcting
+    sheet = wb["Sheet1"]  # sheet name
+    cell = sheet["a1"]  # index number
+    cell = sheet.cell(1, 1)  # rows and colums
+    # print(sheet.max_row)  # for finding how many rows
+    # print(cell.value)
