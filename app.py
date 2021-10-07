052d2f3b93087ce26bcd273afecf417eede65ca5
Mohammed Thasheel <spidy9741@gmail.com>
2021-10-07 20:33:39 +0530

    for file saving

--- app.py
+++ app.py
@@ -13,3 +13,10 @@ def process_workbook(filename):
         corrected_price = cell.value * 0.9
         corrected_price_cell = sheet.cell(row, 4)
         corrected_price_cell.value = corrected_price
+
+        values = Reference(sheet, min_row=2, max_row=sheet.max_row, min_col=4, max_col=4)
+        chart = BarChart()
+        chart.add_data(values)
+        sheet.add_chart(chart, 'e2')
+
+        wb.save(filename)
